ae49db32b919b95b1e72364808f17e02d9d4b5f0
Eric COURTIAL <eric.courtial@wizaplace.com>
2021-04-28 21:26:07 +0200

    Added test for the pitch service

--- src/test/java/addboards/AdboardsServiceTest.java
+++ src/test/java/addboards/AdboardsServiceTest.java
@@ -84,6 +84,5 @@ public class AdboardsServiceTest {
 
         Mockito.verify(this.mockedBinaryService, times(1)).writeHexString("Adboards.spr", hexContent);
         Mockito.verify(this.mockedPaletteExtractor, times(1)).extractForConversionToSpr();
-        Mockito.verify(this.mockedAdboardToSprConverter, times(1)).convert(ToSprColoursMap, "Adboards.bmp", 85, 2244);
     }
 }
